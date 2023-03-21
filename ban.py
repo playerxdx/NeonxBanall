3b8ce15b710ed434f3e1ca0b4cd27a405b183f94
NEO xD <122731828+neolol3@users.noreply.github.com>
2023-03-21 11:41:56 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -50,6 +50,13 @@ print("Starting.....")
 
 Riz = TelegramClient('Riz', Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TOKEN)
 
+START = "https://telegra.ph/file/68d6d0c80c9bb02730e64.jpg"
+
+@register(pattern=("/start"))
+async def awake(event):
+  TEXT = f"ʜɪ ɢᴀʏ [{event.sender.first_name}](tg://user?id={event.sender.id}), ᴛʜɪꜱ ɪꜱ ᴍᴀꜱꜱ ᴀᴄᴛɪᴏɴꜱ ʙᴏᴛ ᴡɪᴛʜ ᴢᴇʀᴏ ᴅᴏᴡɴᴛɪᴍᴇ..!!\n\n **ᴄᴏᴍᴍᴀɴᴅꜱ**\n\n /ping - ᴄʜᴇᴄᴋ ʙᴏᴛꜱ ᴜᴘᴛɪᴍᴇ ! \n /banall - ʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ ꜰʀᴏᴍ ɢʀᴏᴜᴘ \n /unbanall - ᴛᴏ ᴜɴʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ \n\ /kickall - ᴛᴏ ᴋɪᴄᴋ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ"
+  BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇꜱ", "https://t.me/SpotifyxUpdates"),]]
+  await event.send_file(event.chat_id, START, caption=TEXT, buttons=BUTTON)
 
 @Riz.on(events.NewMessage(pattern="^/ping"))  
 async def ping(e):
