7050db8e3288e84edefa1a6cf96a67464e0953e1
NEO xD <122731828+neolol3@users.noreply.github.com>
2023-03-21 12:37:54 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -56,7 +56,7 @@ async def ping(e):
         start = datetime.now()
         text = "Pong!\n\nʜɪ ɢᴀʏ\nᴛʜɪꜱ ɪꜱ ᴍᴀꜱꜱ ᴀᴄᴛɪᴏɴꜱ ʙᴏᴛ ᴡɪᴛʜ ᴢᴇʀᴏ ᴅᴏᴡɴᴛɪᴍᴇ..!!\n\n**ᴄᴏᴍᴍᴀɴᴅꜱ:-**\n\n/banall - ʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ ꜰʀᴏᴍ ɢʀᴏᴜᴘ \n/unbanall - ᴛᴏ ᴜɴʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ \n/kickall - ᴛᴏ ᴋɪᴄᴋ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ!"
         BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇꜱ", "https://t.me/SpotifyxUpdates"),]]
-        event = await e.edit(text, buttons=BUTTON , NEO, parse_mode=None, link_preview=None )
+        event = await e.edit(text, buttons=BUTTON , parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
         await event.edit(f"**ɪ'ᴍ ᴀʟɪᴠᴇ !!** \n\n **ᴘᴏɴɢ** !! `{ms}` ms\n\nʜɪ ɢᴀʏ\nᴛʜɪꜱ ɪꜱ ᴍᴀꜱꜱ ᴀᴄᴛɪᴏɴꜱ ʙᴏᴛ ᴡɪᴛʜ ᴢᴇʀᴏ ᴅᴏᴡɴᴛɪᴍᴇ..!!\n\n**ᴄᴏᴍᴍᴀɴᴅꜱ:-**\n\n/banall - ʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ ꜰʀᴏᴍ ɢʀᴏᴜᴘ \n/unbanall - ᴛᴏ ᴜɴʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ \n/kickall - ᴛᴏ ᴋɪᴄᴋ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ!")
