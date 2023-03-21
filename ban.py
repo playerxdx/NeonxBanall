b52ee831ad5b31539df6ceeb2bfbc265f8d9d248
NEO xD <122731828+neolol3@users.noreply.github.com>
2023-03-21 11:43:52 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -52,7 +52,7 @@ Riz = TelegramClient('Riz', Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TO
 
 START = "https://telegra.ph/file/68d6d0c80c9bb02730e64.jpg"
 
-@register(pattern=("/start"))
+@Riz.on(events.NewMessage(pattern=("/start"))
 async def awake(event):
   TEXT = f"ʜɪ ɢᴀʏ [{event.sender.first_name}](tg://user?id={event.sender.id}), ᴛʜɪꜱ ɪꜱ ᴍᴀꜱꜱ ᴀᴄᴛɪᴏɴꜱ ʙᴏᴛ ᴡɪᴛʜ ᴢᴇʀᴏ ᴅᴏᴡɴᴛɪᴍᴇ..!!\n\n **ᴄᴏᴍᴍᴀɴᴅꜱ**\n\n /ping - ᴄʜᴇᴄᴋ ʙᴏᴛꜱ ᴜᴘᴛɪᴍᴇ ! \n /banall - ʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ ꜰʀᴏᴍ ɢʀᴏᴜᴘ \n /unbanall - ᴛᴏ ᴜɴʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ \n\ /kickall - ᴛᴏ ᴋɪᴄᴋ ᴀʟʟ ᴍᴇᴍʙᴇʀꜱ"
   BUTTON = [[Button.url("ᴜᴘᴅᴀᴛᴇꜱ", "https://t.me/SpotifyxUpdates"),]]
